a7ec62802c424232ff85cf7749783e892cbe1688
Guilherme.Vilela <guilhermesv2002@gmail.com>
2024-07-31 20:53:56 -0300

    ajuste no tempo de time out

--- main/mac.h
+++ main/mac.h
@@ -11,9 +11,9 @@ typedef enum {
 
 
 #define time_transmit 1000000 //
-#define time_out 20000000 // time for desconection
+#define time_out 2000000 // time for desconection
 #define pre_escale_probe_request 5 // 0 a 255 
-#define time_unit 1000 ///definida com 1u / time_unit
+#define time_unit 1 ///definida com 1u / time_unit
 void open_mac_rx_callback(wifi_promiscuous_pkt_t* packet);
 void open_mac_tx_func_callback(tx_func* t);
 
